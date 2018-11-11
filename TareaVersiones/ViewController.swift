81e1a70fa6b90c20c9fd7c0300eb1ad3a14642b4
Dany Alejandro Silva Sanchez <perseo.11@gmail.com>
2018-11-11 12:19:23 -0500

    Initial Commit

---
+++ TareaVersiones/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TareaVersiones
+//
+//  Created by Dany Alejandro on 11/11/18.
+//  Copyright © 2018 Dany Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
